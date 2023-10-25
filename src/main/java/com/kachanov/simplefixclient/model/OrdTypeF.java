7dd170f1d8434f7d5206a7508bfc656251b102db
alexkachanov <alexkachanov@yandex.ru>
2023-10-25 11:34:36 +0900

    updated order types

--- src/main/java/com/kachanov/simplefixclient/model/OrdTypeF.java
+++ src/main/java/com/kachanov/simplefixclient/model/OrdTypeF.java
@@ -1,15 +1,26 @@
 package com.kachanov.simplefixclient.model;
 
 import static quickfix.field.OrdType.LIMIT;
-import static quickfix.field.OrdType.MARKET;
+import static quickfix.field.OrdType.*;
 
 import quickfix.FieldNotFound;
 import quickfix.Message;
 import quickfix.field.OrdType;
 
 public enum OrdTypeF {
-	  Limit(LIMIT)
-	, Market(MARKET)
+	  Market(MARKET) 
+	, Limit(LIMIT)
+	, Stop(STOP_STOP_LOSS)
+	, Stop_limit(STOP_LIMIT)
+	, Market_on_close(MARKET_ON_CLOSE)
+	, With_or_without(WITH_OR_WITHOUT)
+	, Limit_or_better(LIMIT_OR_BETTER)
+	, Limit_with_or_without(LIMIT_WITH_OR_WITHOUT)
+	, On_basis(ON_BASIS)
+	, On_close(ON_CLOSE)
+	, Limit_ob_close(LIMIT_ON_CLOSE)
+	, Funari(FUNARI)
+	, Pegged(PEGGED)
 	;
 	
 	private char _value;
